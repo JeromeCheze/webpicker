afe1788a4029b2d57144560591b73a149611dd60
Jérôme Chèze <cheze@geoazur.unice.fr>
2018-05-22 16:59:35 +0200

    Save work.

--- site_routage.py
+++ site_routage.py
@@ -1,11 +1,41 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from flask import Flask, request, render_template, Response, abort
+from urllib2 import urlopen, Request, HTTPError
+from urllib import urlencode
 import json
+
+app = Flask(__name__)
+app.debug = True
+
+FDSNWS_ROOT = 'http://encelade.unice.fr:8080/fdsnws'
 
 @app.route('/')
 def index():
     return render_template('app.html')
 
+@app.route('/fdsnws/', defaults={'service': '', 'path': ''})
+@app.route('/fdsnws/<service>/', defaults={'path': ''})
+@app.route('/fdsnws/<service>/<path:path>', methods=['GET', 'POST'])
+def fdsnws(service, path):
+    if path != '':
+        path = '/%s' % path
+
+    if service:
+        req = '%s/%s%s' % (FDSNWS_ROOT, service, path)
+        try:
+            if request.method == 'GET':
+                result = urlopen('%s?%s' % (req, urlencode(request.args.to_dict(flat=True))))
+            elif request.method == 'POST':
+                # print(request.data)
+                r = Request(req, data=request.data, headers={'Content-Type': request.headers['Content-Type']})
+                result = urlopen(r)
+
+        except HTTPError, err:
+            abort(err.code)
+        return Response(result.read(), mimetype=result.headers.type)
+    else:
+        return urlopen(FDSNWS_ROOT).read()
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6000)
+    app.run(host='0.0.0.0', port=8001)
